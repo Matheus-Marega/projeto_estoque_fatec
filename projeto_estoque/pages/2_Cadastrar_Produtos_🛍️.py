29fbf23d35d5fbc240fcde62c2d991f8d7e2ca26
Matheus Pansani Marega <142553250+Matheus-Marega@users.noreply.github.com>
2025-05-26 19:29:27 -0300

    Delete projeto_estoque directory

--- projeto_estoque/pages/2_Cadastrar_Produtos_🛍️.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from pages.Funcao_produtos.botao_cadastrar_produto import cadastrar_produto
-
-st.title("Cadastrar Produtos")
-
-nome_produto = st.text_input("Nome do Produto")
-unidade_medida = st.selectbox("Unidade de Medida", ["Kilograma", "Grama", "Litro", "Mililitro","Unidade"])
-marca_produto = st.text_input("Marca do Produto")
-cadastrar_produto(nome_produto, unidade_medida, marca_produto)
